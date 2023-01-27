1faf4280f0a33290021ef9a7197b60cd54e21316
femeunier <felicien.meunier@gmail.com>
2023-01-27 10:30:21 +0100

    Add year exception

--- R/nc.get.time.multiplier.R
+++ R/nc.get.time.multiplier.R
@@ -1,5 +1,5 @@
 nc.get.time.multiplier <- function (x)
 {
   return(switch(x, days = 86400, hours = 3600, minutes = 60,
-                months = 86400 * 30, years = 365*86400, seconds = 1))
+                months = 86400 * 30, years = 365*86400, year = 365*86400, seconds = 1))
 }
