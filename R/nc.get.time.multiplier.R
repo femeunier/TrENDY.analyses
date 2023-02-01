406259d0d394c54d81bc3a57816ef0df9fd4ca4b
femeunier <felicien.meunier@gmail.com>
2023-02-01 12:28:56 +0100

    Correct month

--- R/nc.get.time.multiplier.R
+++ R/nc.get.time.multiplier.R
@@ -1,6 +1,6 @@
 nc.get.time.multiplier <- function (x)
 {
   return(switch(x, days = 86400, hours = 3600, minutes = 60,
-                months = 86400 * 30,  month = 86400 * 30,
+                months = 86400 * 365/12,  month = 86400 * 365/12,
                 years = 365*86400, year = 365*86400, seconds = 1))
 }
