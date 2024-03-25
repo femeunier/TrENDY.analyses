3d0ee62b090b85f5adf365f0e3a3efaeec3f14e1
femeunier <felicien.meunier@gmail.com>
2024-03-25 19:24:20 +0100

    Hack DLEM

--- R/nc.get.time.series.R
+++ R/nc.get.time.series.R
@@ -30,7 +30,11 @@ nc.get.time.series <- function (f, v, time.dim.name, correct.for.gregorian.julia
   time.split <- strsplit(f$dim$time$units, " ")[[1]]
 
   if (length(time.split) == 0){
-    time.split = strsplit("months since 2003-01", " ")[[1]]
+    if (length(f$dim$time$vals) > 1000){
+      time.split = strsplit("months since 2003-01", " ")[[1]]    # DLEM
+    } else {
+      time.split = strsplit("months since 1700-01", " ")[[1]]    # CARDAMOM
+    }
   }
 
   time.res <- time.split[1]
