a06dc1593990c14286562e99e9e628f831cb67c7
femeunier <felicien.meunier@gmail.com>
2025-04-23 10:32:49 +0200

    Add time multiplier

--- R/nc.get.time.multiplier.R
+++ R/nc.get.time.multiplier.R
@@ -2,5 +2,6 @@ nc.get.time.multiplier <- function (x)
 {
   return(switch(x, days = 86400, hours = 3600, minutes = 60,
                 months = 86400 * 365/12,  month = 86400 * 365/12,
-                years = 365*86400, year = 365*86400, seconds = 1))
+                years = 365*86400, year = 365*86400, yr = 365*86400,
+                seconds = 1))
 }
