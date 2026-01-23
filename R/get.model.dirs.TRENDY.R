016ce19b6cec526fe3bbd739529e8fbd3f13787c
Félicien Meunier <felicien@blocked.local>
2026-01-23 16:45:32 +0100

    Names

--- R/get.model.dirs.TRENDY.R
+++ R/get.model.dirs.TRENDY.R
@@ -6,31 +6,31 @@ get.model.dirs.TRENDY <- function(version = "v11"){
                      "LPJ","LPX-Bern","OCN","ORCHIDEE",
                      "SDGVM","VISIT-NIES","VISIT","YIBs")  # v11
   } else if (version == "v12"){
-    model.names <- c("CABLE-POP","CARDAMOM","CLASSIC","CLM5.0",
+    model.dirs <- c("CABLE-POP","CARDAMOM","CLASSIC","CLM5.0",
                      "DLEM","EDv3","E3SM","IBIS","ISAM","ISBA-CTRIP",
                      "JSBACH","JULES","LPJ-GUESS","LPJmL","LPJwsl",
                      "LPX-Bern","OCN","ORCHIDEE","SDGVM","VISIT","YIBs")  # v12
   } else if (version == "v13"){
-    model.names <- c("CABLE-POP","CARDAMOM","CLASSIC","CLM6.0",
+    model.dirs <- c("CABLE-POP","CARDAMOM","CLASSIC","CLM6.0",
                      "DLEM","EDv3","E3SM","IBIS","iMAPLE","ISAM","ISBA-CTRIP",
                      "JSBACH","JULES","LPJ-GUESS","LPJmL","LPJwsl",
                      "LPX-Bern","OCN","ORCHIDEE","SDGVM","VISIT","visit-ut")  # v12
   }  else if (version == "v14"){
-    model.names <- c("CABLE-POP","CARDAMOM","CLASSIC","CLM-FATES","CLM",
+    model.dirs <- c("CABLE-POP","CARDAMOM","CLASSIC","CLM-FATES","CLM",
                      "DLEM","ELM-FATES","ELM","ED",
                      "IBIS","ISAM","ISBA-CTRIP","JULES-ES",
                      "JSBACH","JULES","LPJ-GUESS","LPJml","LPJwsl",
                      "LPX-Bern","OCN","ORCHIDEE","SDGVM","VISIT","TEM","VISIT-UT",
                      "iMAPLE")  # v14
   } else if (version == "Example"){
-    model.names <- c("Example")
+    model.dirs <- c("Example")
   } else {
-    model.names <- c("CLASSIC","CLM5.0","DLEM","IBIS","ISAM","ISBA-CTRIP",
+    model.dirs <- c("CLASSIC","CLM5.0","DLEM","IBIS","ISAM","ISBA-CTRIP",
                      "JSBACH","JULES-ES-1p0","LPJ-GUESS",
                      "LPJ","LPX-Bern","OCN","ORCHIDEE-CNP","ORCHIDEE",
                      "ORCHIDEEv3","SDGVM","VISIT","YIBs")  # v9
   }
 
 
-  return(model.names)
+  return(model.dirs)
 }
